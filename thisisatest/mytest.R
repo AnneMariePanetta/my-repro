6af5bab7cdae310f18502faea4ad45fb23fc70a2
AnneMariePanetta <anne.panetta@gmail.com>
2025-07-16 14:28:58 -0600

    testing AM's writing

--- thisisatest/mytest.R
+++ thisisatest/mytest.R
@@ -1,3 +1,4 @@
 this is a test script
 let's see if we can break it
-because that's useful pedagogically I tried to write something on line3
+because that's useful pedagogically thank you for your help
+
